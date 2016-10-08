e5deba28e8e634d984445395fa4ca29b25330269
Shanjin Yang <shanjin.yang@aispeech.com>
2016-10-08 15:38:50 +0800

    implement file read and write function
    Signed-off-by: Shanjin Yang <shanjin.yang@aispeech.com>

--- main.c
+++ main.c
@@ -7,6 +7,12 @@
 #include <errno.h>  
 #include <string.h>  
 #define BUFFER_SIZE 1024  
+#define print(format, ...) \
+	{ \
+		printf("[%s : %s : %d] ", \
+		__FILE__, __func__, __LINE__); \
+		printf(format, ##__VA_ARGS__); \
+	}
 
 /*return value:  1: file exit
 		-1: file no exit
@@ -44,11 +50,75 @@ end:
 	return retvalue;
 }
 
+/*
+  notice: everytimes call this function will from the file starting position to
+  write.
+*/
+int file_write(int fd, char *content)
+{
+	int retvalue = 1;
+	int size;
+	if ((content == NULL) | (fd <= 0)) {
+		print("error\n\n");
+		retvalue = -1;
+		goto end;
+	}
+
+	/*make sure from the starting position to write*/
+	lseek(fd, 0, SEEK_SET);
+	size = write(fd, content, strlen(content));
+	if (size <= 0) {
+		print("write error\n");
+		retvalue = -1;
+		goto end;
+	}
+end:
+	return retvalue;
+}
+
+/*
+  notice: everytimes call this function will from the file starting position to
+  read.
+*/
+int file_read(int fd, char *buf, int size)
+{
+	int retvalue = 1;
+	if ((buf == NULL) | (fd <= 0) | (size <= 0)) {
+		print("error\n\n");
+		retvalue = -1;
+		goto end;
+	}
+	/*make sure from the starting position to read*/
+	lseek(fd, 0, SEEK_SET);
+	size = read(fd, buf, size);
+	if (size <= 0) {
+		print("read error\n");
+		retvalue = -1;
+		goto end;
+	}
+end:
+	return retvalue;
+}
+
 int main(int argc, char **argv)
 {
-	int retvalue;
-	if (argc != 2)
+	int retvalue = -1;
+	int fd;
+	int size;
+	if (argc != 2) {
+		print("usage: %s file\n", argv[0]);
 		return -1;
-	retvalue = file_create(argv[1]);
-	printf("file create : %d\n", retvalue);
+	}
+	fd = file_create(argv[1]);
+	if (fd == -1) {
+		retvalue = -1;
+		goto end;
+	}
+	file_write(fd, "hell world, my friend\n");
+	lseek(fd, 0, SEEK_SET);
+	char buf[2048] = {0};
+	file_read(fd, buf, 2048);
+	printf("%s", buf);
+end:
+	return retvalue;
 }
