8b5689df23b026c08043f413157de685428512aa
Shanjin Yang <shanjin.yang@aispeech.com>
2016-10-10 16:44:18 +0800

    add json operate test case
    Signed-off-by: Shanjin Yang <shanjin.yang@aispeech.com>

--- SConstruct
+++ SConstruct
@@ -3,4 +3,11 @@ env = Environment(
 	CFLAGS = ['-g', '-Wall', '-I.']
 )
 
-test_case = env.Program("test_case", ['main.c', 'json_op.c', 'fop.c', 'cJSON.c', 'itoa.c'], LIBS='m')
+obj_cJSON = env.Object('cJSON.c')
+obj_itoa = env.Object('itoa.c')
+obj_json_op = env.Object('json_op.c')
+obj_fop = env.Object('fop.c');
+
+json_test_case = env.Program("json_test_case", ['json_op_test_case.c', obj_json_op, obj_cJSON, obj_itoa], LIBS='m')
+
+test_case = env.Program("test_case", ['main.c', obj_json_op, obj_fop, obj_cJSON, obj_itoa], LIBS='m')
---
+++ json_op_test_case.c
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+#include <stdio.h>  
+#include <string.h>
+#include <unistd.h>  
+#include <fcntl.h>
+#include "cJSON.h"
+#include "json_op.h"
+
+int low_output_cb(int arg, char *s, int size)
+{
+	int retvalue = 1;
+	if (s == NULL) {
+		retvalue = -1;
+		goto end;
+	}
+	print("[arg : size : s] [%d : %d : %s]\n", arg, size, s);
+end:
+	return retvalue;
+}
+
+int high_output_cb(char *a, char *b, char *c)
+{
+	int retvalue = 1;
+	if ((a== NULL) || (b == NULL) || (c == NULL)) {
+		retvalue = -1;
+		goto end;
+	}
+	print("[a:b:c] [%s : %s : %s]", a, b, c);
+end:
+	return retvalue;
+}
+
+int low_input_cb(int arg, char *s, int size)
+{
+	int retvalue = 1;
+	if (s == NULL) {
+		retvalue = -1;
+		goto end;
+	}
+
+	int fd = open("./config", O_CREAT | O_RDWR, S_IRWXU | S_IRWXO | S_IRWXG);
+	if (fd <= 0) {
+		retvalue = -1;
+		goto end;
+	}
+	read(fd, s, size);
+
+	print("[arg : size : s] [%d : %d : %s]\n", arg, size, s);
+end:
+	return retvalue;
+}
+
+int op_high_input_func(struct op *o)
+{
+	int i;
+	int retvalue = 1;
+	char str[3] = {0};
+	for (i = 0; i < 20; i++) {
+		itoa(i, str, 10);
+		retvalue = op_high_input(i, o, str, "b", str);
+		if (retvalue == -1) {
+			goto end;
+		}
+	}
+end:
+	return retvalue;
+}
+
+int main()
+{
+	struct op *o;
+	op_init(&o, 0);
+	op_reg_low_output(o, low_output_cb);
+	op_reg_high_output(o, high_output_cb);
+	op_reg_low_input(o, low_input_cb);
+
+	op_low_input(o);
+	int i;
+	for (i = 0; i < 100; i++)
+		op_high_output(o, i);
+
+	op_high_input_func(o);
+	op_low_output(o);
+
+	op_delete(&o);
+	return 0;
+}
