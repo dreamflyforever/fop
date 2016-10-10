2344e72a8a6907d02595b7b9f3b7aed530aa1755
Shanjin Yang <shanjin.yang@aispeech.com>
2016-10-10 14:11:28 +0800

    fix the itoa function buf
    fixed: When enter negative number, itoa will output wrong number.
    
    Signed-off-by: Shanjin Yang <shanjin.yang@aispeech.com>

--- itoa.c
+++ itoa.c
@@ -1,53 +1,71 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-char *itoa(int num,char *str,int radix)
+/* A utility function to reverse a string  */
+void reverse(char str[], int length)
 {
-	char *retvalue;
-	if (str == NULL) {
-		retvalue = NULL;
-		goto
-			end;
+	int start = 0;
+	int end = length -1;
+	char tmp;
+	while (start < end) {
+		tmp = *(str + start);
+		*(str+start) = *(str+end);
+		*(str + end) = tmp;
+		start++;
+		end--;
 	}
-
-	char index[]="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
-	unsigned unum;
-	int i = 0, j, k; 
-	if ((radix == 10) && (num < 0)) {
-		unum=(unsigned)-num; 
-		str[i++]='-'; 
-	}  else {
-		unum = (unsigned)num;
-	}
-
-	do { 
-		str[i++] = index[unum % (unsigned)radix]; 
-		unum /= radix; 
-	} while(unum); 
-	str[i]='\0'; 
-	if (str[0]=='-') k=1;
-	else k=0; 
-	char temp; 
-	for (j = k; j <= (i-k-1)/2.0; j++) { 
-		temp=str[j]; 
-		str[j] = str[i-j-1]; 
-		str[i-j-1] = temp; 
-	}
-	retvalue = str;
-end:
-	return retvalue; 
 }
 
+char* itoa(int num, char* str, int base)
+{
+	if (str == NULL) {
+		return NULL;
+	}
+#define false 0
+#define true 1
+	int i = 0;
+	int isNegative = false;
+
+	/* Handle 0 explicitely, otherwise empty string is printed for 0 */
+	if (num == 0) {
+		str[i++] = '0';
+		str[i] = '\0';
+		return str;
+	}
+
+	/* In standard itoa(), negative numbers are handled only with 
+	 base 10. Otherwise numbers are considered unsigned.*/
+	if (num < 0 && base == 10) {
+		isNegative = true;
+		num = -num;
+	}
+
+	/* Process individual digits */
+	while (num != 0) {
+		int rem = num % base;
+		str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0';
+		num = num/base;
+	}
+
+	/*If number is negative, append '-'*/
+	if (isNegative)
+		str[i++] = '-';
+
+	str[i] = '\0'; /*Append string terminator*/
+
+	/*Reverse the string*/
+	reverse(str, i);
+
+	return str;
+}
 #if 0
 int main()
 {
-	int num = 0;
-	char str[25];
+	char str[3];
 	int i;
-	for (i = 0; i < 100; i++) {
-		itoa(num, str, 10);
-		printf("%s\n", str);
-		num++;
+	for (i = -100; i < 100; i++) {
+		itoa(i, str, 10);
+		printf("[%d] %s\t", i, str);
 	}
 	return 0;
 }
